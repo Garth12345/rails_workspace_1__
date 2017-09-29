8d13206f1cb4e769fdf60bd179c1367bbc5e06c2
Garth Adcock <garthadcock1@gmail.com>
2017-09-29 21:19:31 +0000

    fin toy_app

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,15 @@
 class StaticPagesController < ApplicationController
+  Rails.application.routes.draw do
+  end
   def home
   end
+#root :to => "static#home"
+#root :to => "static#about"
+#root :to => "static#help"
+
 
   def help
+  end
+  def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,34 @@
 Rails.application.routes.draw do
+  get 'my_pages/about'
+
+  get 'my_pages/home'
+
+  get 'my_pages/help'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
   resources :microposts
   resources :users
-  root 'users#index'
+  #root 'users#index'
+  root 'my_pages#home'
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  get 'my_pages/about'
+
+  get 'my_pages/home'
+
+  get 'my_pages/help'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
+  get "/:page" => "static#show"
 
   resources :microposts
-  root 'application#hello'
+  #root 'application#home'
+  #root 'application#hello'
 end
-
